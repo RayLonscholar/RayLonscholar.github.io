185bb194bf0d736fdb0bb435ab9ff8909aad0752
RayLonscholar <f82621672@gmail.com>
2024-10-04 22:00:42 +0800

    Site updated: 2024-10-04 22:00:41

---
+++ js/typed.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var options = {
+      strings: ["這是第一行文字", "這是第二行文字"],
+      typeSpeed: 50
+    };
+  
+    var typed = new Typed(".element", options);
+  });
+  
